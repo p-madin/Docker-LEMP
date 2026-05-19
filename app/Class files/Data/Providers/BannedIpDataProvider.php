74579d79ae399ecfce66208e311977ee7c8ad1d9
Phil Madin <p.madin@hotmail.com>
2026-05-19 16:50:39 +1000

    refabricate post param

--- app/Class files/Data/Providers/BannedIpDataProvider.php
+++ app/Class files/Data/Providers/BannedIpDataProvider.php
@@ -5,14 +5,14 @@ class BannedIpDataProvider implements DataProviderInterface {
             ['key' => 'biIP', 'label' => 'IP Address'],
             ['key' => 'biDateAdded', 'label' => 'Banned At'],
             ['key' => 'biReason', 'label' => 'Reason'],
-            ['key' => 'actions', 'label' => 'Actions', 'action' => 'button_form', 'actionConfig' => ['url' => '/unban_ip', 'params' => ['ip' => 'biIP'], 'buttonLabel' => 'Unban']]
+            ['key' => 'actions', 'label' => 'Actions', 'action' => 'button_form', 'actionConfig' => ['url' => '/unban_ip', 'params' => ['biPK' => 'biPK'], 'buttonLabel' => 'Unban']]
         ];
     }
 
     public function getData(): array {
         global $db, $dialect;
         $qb = new QueryBuilder($dialect);
-        return $qb->table('banned_ips')->select(['biIP', 'biDateAdded', 'biReason'])->getFetchAll($db);
+        return $qb->table('banned_ips')->select(['biPK', 'biIP', 'biDateAdded', 'biReason'])->getFetchAll($db);
     }
 
     public function getNestedKey(): ?string {
